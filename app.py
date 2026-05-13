195e1c748862684e043ed4eef7b34b7d5b45555c
makelegalsolutions <makelegalsolutions@gmail.com>
2026-05-13 12:04:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def search_law(
 st.title("⚖️ AI giải đáp pháp luật VN? - Chính tôi!")
 
 st.caption(
-    "Bạn cần gì cứ nói nhé, vô tư đi. Tôi không bao giờ nói nhiều, không bao giờ ép ăn và không bao giờ ép học. Hệ thống demo, đang cập nhật thêm dữ liệu"
+    "Bạn cần gì cứ nói nhé, vô tư đi. Tôi không bao giờ nói nhiều, không bao giờ ép ăn và không bao giờ ép học. /<BR> Hệ thống demo, đang cập nhật thêm dữ liệu"
 )
 
 question = st.text_area(
